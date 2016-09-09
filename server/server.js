67d1885567982f7b59e45cbab262bb573b090b58
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-08 19:48:42 -0500

    install dev dependencies, create test file, create package.json

--- server/server.js
+++ server/server.js
@@ -1,22 +1,9 @@
-let http = require('http');
+var http = require('http');
 
-let port = 8080;
+var port = 8080;
 
-let ip = "127.0.0.1";
+var ip = "127.0.0.1";
 
-let server = http.createServer(function(req,res){
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("<!DOCTYPE html>");
-  res.write("<html>");
-  res.write("<head>");
-  res.write("<title>Hello World Page</title>");
-  res.write("</head>");
-  res.write("<body>");
-  res.write("Hello World!");
-  res.write("</body>");
-  res.write("</html>");
-  res.end();
-})
 
 console.log("Listening on http://localhost:" + port);
 server.listen(port, ip);
