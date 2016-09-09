1c37fc41e4e34e2b0bdb081cd55a8e58533483fc
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-09 17:43:07 -0500

    Write tests for js file and html file

--- spec/serverTest.js
+++ spec/serverTest.js
@@ -3,13 +3,21 @@ var expect = require('chai').expect;
 
 
 describe('server', function() {
-  it('should respond with something',function(done) {
-  	 request('http://localhost:8080', function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      console.log(body)
-      done();
-    });
-  });
+	// Test to make sure Html file is sent when requested.
+	it('should respond with a status of 200 for main page',function(done) {
+  		request('http://localhost:8080', function(error, response, body) {
+      		expect(response.statusCode).to.equal(200);
+      		expect()
+      		done();
+   		});
+  	});
+  	// Test to make sure JS file is sent when requested. 
+	it('should respond with a status of 200 when asked for main.js', function(done){
+  		request('http://localhost:8080/main.js', function(error,response, body){
+  			expect(response.statusCode).to.equal(200);
+  			done();
+  		})
+ 	})
 
 
 
