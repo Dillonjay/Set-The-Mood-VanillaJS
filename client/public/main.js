6f094e5e874eb90ddc9a1eafb7e94f5b8234e7ba
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-09 18:15:02 -0500

    send out first ajax using vanilla js

--- client/public/main.js
+++ client/public/main.js
@@ -1,1 +1,16 @@
-alert('fun JS')
+
+// Ajax request without Jquery/ 
+document.querySelector("h1").addEventListener("click", function(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://localhost:8080');
+	xhr.onload = function() {
+    if (xhr.status === 200) {
+        alert(xhr.responseText);
+    }
+    else {
+        alert('Request failed.  Returned status of ' + xhr.status);
+    }
+};	
+// Send the request
+xhr.send()
+})
