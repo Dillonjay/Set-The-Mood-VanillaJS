cceb95be35c2afef3242d630a3e828687f6eb855
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-09 17:10:11 -0500

    basic testing set up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+// Middleware for parsing any incoming json.
+app.use(bodyParser.json());
+// Middleware for parsing any incoming cookies.
+app.use(cookieParser());
 
 
 
 app.get('/', function(req, res){
-  res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+  res.sendStatus(200)
+  // res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
 });
 
 app.listen(8080);
--- spec/serverTest.js
+++ spec/serverTest.js
@@ -1,2 +1,15 @@
 var request = require('request');
-var expect = require('chai').expect()
+var expect = require('chai').expect;
+
+
+describe('server', function() {
+  it('should respond with something',function(done) {
+  	 request('http://localhost:8080', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+
+
+});
