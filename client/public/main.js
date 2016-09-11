1635ce1f03b74c07e4f988110cac016e6ecb1eb7
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-11 18:05:03 -0500

    add input for user to search spotify. Set up post request to server on search

--- client/public/main.js
+++ client/public/main.js
@@ -1,2 +1,22 @@
 
-alert('its ALIVEEe')
+// Ajax request without Jquery.
+
+document.querySelector(".search") ? 
+document.querySelector(".search").addEventListener('click', function(e) {
+	e.preventDefault;
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', '/search');
+	xhr.onload = function() {
+    if (xhr.status === 200) {
+        alert(xhr.responseText);
+    }
+    else {
+        alert('Request failed.  Returned status of ' + xhr.status);
+    }
+};	
+// Send the request
+xhr.send()
+})
+: null;
+
+//Ajax request to authenticate a spotify user.
